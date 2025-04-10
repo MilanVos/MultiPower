f31486c10a9f3d5a2b5b6f87c804831283d62703
Milan <MilanV2005@outlook.com>
2025-04-10 11:21:37 +0200

    ...

--- src/main/java/nl/multitime/multiPower/MultiPower.java
+++ src/main/java/nl/multitime/multiPower/MultiPower.java
@@ -1,17 +1,51 @@
 package nl.multitime.multiPower;
 
+import nl.multitime.multiPower.commands.PowerCommand;
+import nl.multitime.multiPower.listeners.GUIListener;
+import nl.multitime.multiPower.listeners.PlayerJoinListener;
+import nl.multitime.multiPower.listeners.PowerAbilityListener;
+import nl.multitime.multiPower.managers.PowerManager;
+import nl.multitime.multiPower.powers.FirePower;
+import nl.multitime.multiPower.powers.WaterPower;
+import nl.multitime.multiPower.powers.EarthPower;
+import nl.multitime.multiPower.powers.AirPower;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MultiPower extends JavaPlugin {
 
+    private PowerManager powerManager;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        saveDefaultConfig();
 
+        powerManager = new PowerManager(this);
+
+        registerPowers();
+
+        getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
+        getServer().getPluginManager().registerEvents(new PowerAbilityListener(this), this);
+        getServer().getPluginManager().registerEvents(new GUIListener(this), this);
+
+        getCommand("power").setExecutor(new PowerCommand(this));
+
+        getLogger().info("MultiPower is enabled!");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        powerManager.saveAllPlayerData();
+        getLogger().info("MultiPower is disabled!");
+    }
+
+    private void registerPowers() {
+        powerManager.registerPower(new FirePower());
+        powerManager.registerPower(new WaterPower());
+        powerManager.registerPower(new EarthPower());
+        powerManager.registerPower(new AirPower());
+    }
+
+    public PowerManager getPowerManager() {
+        return powerManager;
     }
 }
